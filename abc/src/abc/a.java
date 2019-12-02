a985d1c3629d50961ba5e0c6d57755d834997a14
tedu <tedu@DESKTOP-5FTQJJE>
2019-12-02 17:00:23 +0800

    master

--- abc/src/abc/a.java
+++ abc/src/abc/a.java
@@ -3,8 +3,10 @@ package abc;
 public class a {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
+	public void test() {
+		System.out.println("实例方法");
+	}
 }
